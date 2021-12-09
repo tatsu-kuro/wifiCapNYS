978a3bc6fa1872ba533d40143d3f7f44fa8a249a
tatsuaki <tatsuakikuroda@me.com>
2021-12-10 07:53:37 +0900

    delete gomi

--- iCapNYS/ViewController.swift
+++ iCapNYS/ViewController.swift
@@ -17,13 +17,11 @@ import AssetsLibrary
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     let someFunctions = myFunctions()
-//    let album = AlbumController()
     let TempFilePath: String = "\(NSTemporaryDirectory())temp.mp4"
     let albumName:String = "iCapNYS"
     var videoArrayCount:Int = 0
     var videoDate = Array<String>()
     var videoURL = Array<URL>()
-//    var albumExist:Bool=false
     @IBOutlet weak var how2Button: UIButton!
     @IBOutlet weak var cameraButton: UIButton!
     
@@ -46,11 +44,9 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
             UserDefaults.standard.set(bottomPadding,forKey: "bottomPadding")
             UserDefaults.standard.set(leftPadding,forKey: "leftPadding")
             UserDefaults.standard.set(rightPadding,forKey: "rightPadding")
-//            print("in viewDidLayoutSubviews")
             let left=UserDefaults.standard.integer(forKey:"leftPadding")
             print("top,bottom,right,left,(int Left)",topPadding,bottomPadding,rightPadding,leftPadding,left)    // iPhoneXなら44, その他は20.0
         }
-//        setButtons()
     }
     override var prefersStatusBarHidden: Bool {
         return true
@@ -89,21 +85,12 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     }
     
     @IBAction func unwindAction(segue: UIStoryboardSegue) {
-        //        camera.getAlbumList()
-        //        tableView.reloadData()
-        //        videoArrayCount=ca.videoURL.count
+
         if let vc = segue.source as? RecordViewController{
             let Controller:RecordViewController = vc
             if Controller.stopButton.isHidden==true{//Exit
                 print("Exit")
-                //            }else{//recorded
-                //                while Controller.saved2album == false{//albumに保存されるのを待つ
-                //                    sleep(UInt32(0.1))
-                //                }
-                //                getAlbumList()
-                //                print("recorded")
-                //                tableView.reloadData()
-                //                videoArrayCount=videoURL.count
+           
             }
             print("segue:","\(segue.identifier!)")
             Controller.motionManager.stopDeviceMotionUpdates()
@@ -113,72 +100,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         UIApplication.shared.isIdleTimerDisabled = false//スリープする.監視する
         print("unwi")
     }
-    //アルバムの一覧取得
- /*   var gettingAlbumF:Bool=true
-    func getAlbumList(){//最後のvideoを取得するまで待つ
-        gettingAlbumF = true
-        getAlbumList_sub()
-        while gettingAlbumF == true{
-            sleep(UInt32(0.1))
-        }
-    }
-    func getAlbumList_sub(){
-        //     let imgManager = PHImageManager.default()
-        let requestOptions = PHImageRequestOptions()
-        videoURL.removeAll()
-        videoDate.removeAll()
-        requestOptions.isSynchronous = true
-        requestOptions.isNetworkAccessAllowed = false//これでもicloud上のvideoを取ってしまう
-        requestOptions.deliveryMode = .highQualityFormat
-        // アルバムをフェッチ
-        let assetFetchOptions = PHFetchOptions()
-        assetFetchOptions.predicate = NSPredicate(format: "title == %@", albumName)
-        let assetCollections = PHAssetCollection.fetchAssetCollections(with: .album, subtype: .smartAlbumVideos, options: assetFetchOptions)
-        
-        //アルバムが存在しない事もある？
-        if (assetCollections.count > 0) {
-            //同じ名前のアルバムは一つしかないはずなので最初のオブジェクトを使用
-            let assetCollection = assetCollections.object(at:0)
-            // creationDate降順でアルバム内のアセットをフェッチ
-            let fetchOptions = PHFetchOptions()
-            fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
-            let assets = PHAsset.fetchAssets(in: assetCollection, options: fetchOptions)
-//            albumExist=true
-            if assets.count == 0{
-                gettingAlbumF=false
-//                albumExist=false
-            }
-            let formatter = DateFormatter()
-            formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-            for i in 0..<assets.count{
-                let asset=assets[i]                
-                let date_sub = asset.creationDate
-                let date = formatter.string(from: date_sub!)
-                let duration = String(format:"%.1fs",asset.duration)
-                let options=PHVideoRequestOptions()
-                options.version = .original
-                PHImageManager.default().requestAVAsset(forVideo:asset,
-                                                        options: options){ [self](asset:AVAsset?,audioMix, info:[AnyHashable:Any]?)->Void in
-                    
-                    if let urlAsset = asset as? AVURLAsset{//not on iCloud
-                        videoURL.append(urlAsset.url)
-                        videoDate.append(date + "(" + duration + ")")
-                        if i == assets.count - 1{
-                            gettingAlbumF=false
-                        }
-                    }else{//on icloud
-                        if i == assets.count - 1{
-                            gettingAlbumF=false
-                        }
-                    }
-                }
-            }
-        }else{
-//            albumExist=false
-            gettingAlbumF=false
-        }
-    }
-*/
+ 
     func camera_alert(){
         if PHPhotoLibrary.authorizationStatus() != .authorized {
             PHPhotoLibrary.requestAuthorization { status in
@@ -224,9 +146,6 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         let mainBrightness = UIScreen.main.brightness
         UserDefaults.standard.set(mainBrightness, forKey: "mainBrightness")
 
-//        how2Button.layer.borderColor = UIColor.black.cgColor
-//        how2Button.layer.borderWidth = 1.0
-//        how2Button.layer.cornerRadius = 10
         let leftPadding=CGFloat( UserDefaults.standard.integer(forKey:"leftPadding"))
         let rightPadding=CGFloat(UserDefaults.standard.integer(forKey:"rightPadding"))
         let topPadding=CGFloat(UserDefaults.standard.integer(forKey:"topPadding"))
@@ -238,7 +157,6 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         let bh=bw*170/440
         let by=wh-bh-sp
         let by0=topPadding+sp
-//        let by1=wh-(bh+sp)*2
         let x0=leftPadding+sp*2
         
         someFunctions.setButtonProperty(how2Button, x:x0+bw*6+sp*6, y: by, w: bw, h: bh, UIColor.darkGray)
@@ -251,84 +169,14 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
 
         cameraButton.frame=CGRect( x: view.bounds.width-rightPadding-2*sp-wh+2*bh, y: topPadding+bh, width:wh-2*bh, height: wh-2*bh)
         tableView.frame = CGRect(x:leftPadding,y:topPadding+sp,width: view.bounds.width-rightPadding-2*sp-wh+2*bh-leftPadding,height: wh-2*sp)
-//        camera.setLabelProperty(focusLabel,x:x0,y:by,w:bw,h:bh,UIColor.darkGray)
-//        focusBar.frame = CGRect(x:x0+bw+sp, y: by, width:bw*2+sp, height: bh)
-//        camera.setLabelProperty(lightLabel,x:x0,y:by1,w:bw,h:bh,UIColor.darkGray)
-//        lightBar.frame = CGRect(x:x0+bw+sp,y:by1,width:bw*2+sp,height:bh)
-//
-//        camera.setLabelProperty(zoomLabel,x:x0+bw*3+sp*3,y:by,w:bw,h:bh,UIColor.darkGray)
-//        zoomBar.frame = CGRect(x:x0+bw*4+sp*4,y:by,width:bw*2+sp,height: bh)
-//        camera.setButtonProperty(exitButton,x:x0+bw*6+sp*6,y:by,w:bw,h:bh,UIColor.darkGray)
-//        camera.setButtonProperty(cameraChangeButton,x:x0+bw*6+sp*6,y:by1,w:bw,h:bh,UIColor.darkGray)
-////        speakerSwitch.frame = CGRect(x:x0+bw*4+sp*4,y:y1,width:bw,height:bh)
-//        speakerSwitch.frame = CGRect(x:x0+bw*4*sp*4,y:by1,width:bw,height:bh)
-////        //switchの大きさは規定されているので、作ってみてそのサイズを得て、再設定
-//        let switchWidth=speakerSwitch.frame.width
-//        let switchHeight=speakerSwitch.frame.height
-//        let d=(bh-switchHeight)/2
-//        speakerSwitch.frame = CGRect(x:x0+bw*4+sp*4,y:by1+d,width:switchWidth,height: bh)
-//        speakerLabel.frame = CGRect(x:x0+bw*4+sp*4+switchWidth,y:by1,width:bw/2,height:bh)
-//        setProperty(label: currentTime, radius: 2)
-//        currentTime.font = UIFont.monospacedDigitSystemFont(ofSize: view.bounds.width/20, weight: .medium)
-//        currentTime.frame = CGRect(x:x0+sp*6+bw*6, y: sp, width: bw, height: bh)
-//
 
-//        let album = AlbumController()
-//        album.getAlbumList()
-//        getAlbumList()
-//        videoArrayCount = album.videoURL.count
-//        tableView.reloadData()
         UIApplication.shared.isIdleTimerDisabled = false//スリープする
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         print("viewwillappear")
     }
- /*   //nuber of cell
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if album.albumExist==false{
-            return 0
-        }else{
-//            let album = AlbumController()
-            return album.videoURL.count
-        }
-    }
-    //set data on cell
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath:IndexPath) -> UITableViewCell{
-        album.getAlbumList()
 
-        videoArrayCount=album.videoURL.count
-        
-        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier:"cell",for :indexPath)
-        let number = (indexPath.row+1).description + ") "
-        cell.textLabel!.text = number + album.videoDate[indexPath.row]
-        return cell
-    }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let storyboard: UIStoryboard = self.storyboard!
-        let nextView = storyboard.instantiateViewController(withIdentifier: "playView") as! PlayViewController
-        nextView.videoURL = album.videoURL[indexPath.row]
-        self.present(nextView, animated: true, completion: nil)
-    }
-    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        return false
-    }//not sort
-    
-    //セルの削除ボタンが押された時の処理
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        //削除するだけなのでindexPath_row = indexPath.rowをする必要はない。
-        if editingStyle == UITableViewCell.EditingStyle.delete {
-            album.eraseVideo(number: indexPath.row)
-            while album.dialogStatus==0{
-                sleep(UInt32(0.1))
-            }
-            if album.dialogStatus==1{
-                album.videoURL.remove(at: indexPath.row)
-                album.videoDate.remove(at: indexPath.row)
-                tableView.reloadData()
-            }
-        }
-    }*/
     //nuber of cell
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
             return someFunctions.videoURL.count
