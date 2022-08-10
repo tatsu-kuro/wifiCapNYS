fac9f278f98205f1601b63d666b3133fcf78167b
tatsuaki <tatsuakikuroda@me.com>
2022-08-10 10:28:54 +0900

    steel3.movの変更

--- iCapNYS/AutoRecordViewController.swift
+++ iCapNYS/AutoRecordViewController.swift
@@ -475,7 +475,7 @@ class AutoRecordViewController: UIViewController, AVCaptureVideoDataOutputSample
                 print("stop")
                 exitButton.alpha=0.5
                 onClickStopButton()
-                playMoviePath("steel3")
+                playMoviePath("steel3new")
 //                videoView.frame = self.view.bounds
             }
             if movieTimerCnt == 25+22+1{
@@ -527,7 +527,7 @@ class AutoRecordViewController: UIViewController, AVCaptureVideoDataOutputSample
             if movieTimerCnt == 100+12+23+23+22+25{
                 exitButton.alpha=0.5
                 onClickStopButton()
-                playMoviePath("steel3")
+                playMoviePath("steel3new")
 //                videoView.frame = self.view.bounds
             }
             if movieTimerCnt == 100+12+23+23+22+25+1{
